42e566971717d2e2fc8195b1e2f815a4118f0c00
RyosukeMiyamoto <yahsan2@gmail.com>
2020-12-01 21:00:57 +0900

    update

--- js/predict.js
+++ js/predict.js
@@ -41,8 +41,8 @@ function startWebcam() {
   tracker.setInitialScale(4);
   tracker.setStepSize(2);
   tracker.setEdgesDensity(0.1);
-  tracking.track(video, tracker, { camera: true });
   video.play()
+  tracking.track(video, tracker, { camera: true });
 };
 
 //-----------------------
