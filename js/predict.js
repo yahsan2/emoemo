fa630e8c78c00768d09d183ab2ab1019671fec50
RyosukeMiyamoto <yahsan2@gmail.com>
2020-12-03 23:19:02 +0900

    update

--- js/predict.js
+++ js/predict.js
@@ -1,161 +1,124 @@
 const CLASSES =  ({0:'😠 angry',1:'😬 disgust',2:'😨 fear',3:'😄 happy', 4:'😢 sad',5:'😮 surprise',6:'😐 neutral'})
 const COLORS =  ({0:'red',1:'green',2:'purple',3:'yellow', 4:'blue',5:'skyblue',6:'white'})
 
-// MediaDevices.getUserMedia() default size 640pxx480px
 let originalVideoWidth=640;
 
 let model;
 let emotion= [6,CLASSES[6],1.000000];
 let emotionColor;
 
-//-----------------------
-// start button event
-//-----------------------
-
-$("#start-button").click(function (){
-  loadModel();
-  startWebcam();
-});
-
-//-----------------------
-// load model
-//-----------------------
-
-async function loadModel(){
-  console.log("model loading..");
-
-  $("#console").html(`<li>model loading...</li>`);
-  model = await tf.loadModel(`/emotion_XCEPTION/model.json`);
-
-  console.log("model loaded.");
-  $("#console").html(`<li>XCEPTION model loaded.</li>`);
-};
-
-//-----------------------
-// start webcam 
-//-----------------------
-var video = $('#main-stream-video').get(0);
 var tracker = new tracking.LandmarksTracker();
 
-function startWebcam() {
-  tracker.setInitialScale(4);
-  tracker.setStepSize(2);
-  tracker.setEdgesDensity(0.1);
-  video.play()
-  tracking.track(video, tracker, { camera: true });
-};
+Vue.component('monitor-canvas', {
+  template: '<canvas id="faceCanvas" width="320" height="240" ></canvas>',
+})
+Vue.component('monitor-video', {
+  template: '<video width="320" height="240" id="monitor-video" loop preload playsinline autoplay></video>',
+})
 
-//-----------------------
-// predict button event
-//-----------------------
+new Vue({
+  el: '#app',
+  vuetify: new Vuetify(),
+  data() {
+    return {
+      model: null,
+      debug_message: 'hello.please load model.'
+    }
+  },
+  methods: {
+    start: function() {
+      this.startWebcam();
+      this.alignment();
+    },
+    startWebcam() {
+      var $video = this.$refs.monitor.$el
+      tracker.setInitialScale(4);
+      tracker.setStepSize(2);
+      tracker.setEdgesDensity(0.1);
+      $video.play()
+      tracking.track($video, tracker, { camera: true });
+    },
+    alignment() {
+      tracker.on('track', (event)=> {
+        var canvas = this.$refs.canvas.$el;
+        var context = canvas.getContext('2d');
 
-$("#predict-button").click(function (){
-  alignment();
-});
+        context.clearRect(0,0, canvas.width, canvas.height);
+        if(!event.data) return;
+        event.data.faces.forEach((rect)=> {
+          this.predict(rect);
+          console.log(emotion[0],emotion[1],emotion[2]);
 
-//-----------------------
-// face alignment
-//-----------------------
+          emotionColor = COLORS[emotion[0]]
 
-async function alignment(){
-
-  tracker.on('track', function(event) {
-    var canvas = $('#main-stream-canvas').get(0);
-    var context = canvas.getContext('2d');
-
-    context.clearRect(0,0,canvas.width, canvas.height);
-    if(!event.data) return;
-      event.data.faces.forEach(function(rect) {
-        predict(rect);
-        console.log(emotion[0],emotion[1],emotion[2]);
-
-        emotionColor = COLORS[emotion[0]]
-
-        context.strokeStyle = emotionColor;
-        context.lineWidth = 2;
-        context.strokeRect(rect.x, rect.y, rect.width, rect.height);
-        context.font = '11px Helvetica';
-        context.fillStyle = emotionColor;
-        context.fillText(emotion[1], rect.x + rect.width + 5, rect.y + 11);
-        context.fillText(emotion[2].toFixed(6), rect.x + rect.width + 5, rect.y + 22);
-
-      });
-      event.data.landmarks.forEach(function(landmarks) {
-        for(var l in landmarks){
-          context.beginPath();
+          context.strokeStyle = emotionColor;
+          context.lineWidth = 2;
+          context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+          context.font = '11px Helvetica';
           context.fillStyle = emotionColor;
-          context.arc(landmarks[l][0],landmarks[l][1],2,0,2*Math.PI);
-          context.fill();
-        }
+          context.fillText(emotion[1], rect.x + rect.width + 5, rect.y + 11);
+          context.fillText(emotion[2].toFixed(6), rect.x + rect.width + 5, rect.y + 22);
+
+        });
+        event.data.landmarks.forEach(function(landmarks) {
+          for(var l in landmarks){
+            context.beginPath();
+            context.fillStyle = emotionColor;
+            context.arc(landmarks[l][0],landmarks[l][1],2,0,2*Math.PI);
+            context.fill();
+          }
+        });
       });
-});
-};
+    },
+    preprocessImage(image){
+      const channels = 1;
+      let tensor = tf.fromPixels(image, channels).resizeNearestNeighbor([64,64]).toFloat();
+      let offset = tf.scalar(255);
+      return tensor.div(offset).expandDims();
+    },
+    captureWebcam: function(rect) {
+      console.log(this.$refs)
+      var faceCanvas = this.$refs.faceCanvas;
+      var faceContext = faceCanvas.getContext('2d');
+      var video = this.$refs.monitor.$el
 
-//-----------------------
-// TensorFlow.js method
-// predict tensor
-//-----------------------
+      var adjust = originalVideoWidth / video.width
+      faceContext.drawImage(video, rect.x * adjust , rect.y * adjust, rect.width * adjust, rect.height * adjust,0, 0, 100, 100);
 
-async function predict(rect){
+      tensor_image = this.preprocessImage(faceCanvas);
 
-  let tensor = captureWebcam(rect) ;
+      return tensor_image;
+    },
+    async predict(rect){
+      let tensor = this.captureWebcam(rect) ;
 
-  let prediction = await model.predict(tensor).data();
-  let results = Array.from(prediction)
-              .map(function(p,i){
-  return {
-      probability: p,
-      className: CLASSES[i],
-      classNumber: i
-  };
-  }).sort(function(a,b){
-      return b.probability-a.probability;
-  }).slice(0,6);
-
-  $("#console").empty();
-  results.forEach(function(p){
-      $("#console").append(`<li>${p.className} : ${p.probability.toFixed(6)}</li>`);
-  
-  return emotion = [results[0].classNumber,results[0].className, results[0].probability] 
-
-});
-};
-
-//------------------------------
-// capture streaming video 
-// to a canvas object
-//------------------------------
-
-function captureWebcam(rect) {
-  var faceCanvas = $('#faceCanvas').get(0);
-  var faceContext = faceCanvas.getContext('2d');
-
-  //adjust original video size
-  var adjust = originalVideoWidth / video.width
-  faceContext.drawImage(video, rect.x * adjust , rect.y * adjust, rect.width * adjust, rect.height * adjust,0, 0, 100, 100);
-
-	tensor_image = preprocessImage(faceCanvas);
-
-	return tensor_image;
-}
-
-//-----------------------
-// TensorFlow.js method
-// image to tensor
-//-----------------------
-
-function preprocessImage(image){
-  const channels = 1;
-  let tensor = tf.fromPixels(image, channels).resizeNearestNeighbor([64,64]).toFloat();
-  let offset = tf.scalar(255);
-  return tensor.div(offset).expandDims();
-};
-
-
-//-----------------------
-// clear button event
-//-----------------------
-
-$("#clear-button").click(async function clasify(){
-  location.reload();
-});
+      let prediction = await this.model.predict(tensor).data();
+      let results = Array.from(prediction)
+                  .map(function(p,i){
+      return {
+          probability: p,
+          className: CLASSES[i],
+          classNumber: i
+      };
+      }).sort(function(a,b){
+          return b.probability-a.probability;
+      }).slice(0,6);
+      //   $("#console").empty();
+      results.forEach((p)=>{
+        // $("#console").append(`<li>${p.className} : ${p.probability.toFixed(6)}</li>`);
+        console.log(`<li>${p.className} : ${p.probability.toFixed(6)}</li>`)
+        this.debug_message = `${p.className} : ${p.probability.toFixed(6)}`
+        return emotion = [results[0].classNumber,results[0].className, results[0].probability]
+      });
+    },
+    async loadModel(){
+      console.log("model loading..");
+      this.debug_message = `model loading...`
+      this.model = await tf.loadModel(`/emotion_XCEPTION/model.json`);
+      this.debug_message = `XCEPTION model loaded.`
+    }
+  },
+  mounted() {
+    this.loadModel();
+  },
+})
